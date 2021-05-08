9314f6261bb69420b3f9c7dc445e2bd78205b1d4
Koushik <rskkoushik@gmail.com>
2021-05-08 13:12:46 +0530

    feat: update css segment

--- src/views/CSS.js
+++ src/views/CSS.js
@@ -4,7 +4,57 @@ import Details from "../shared/Details";
 const CSS = () => {
   const contents = [
     {
-      title: "Typography - em, rem",
+      title: 'CSS Specificity'
+    },
+    {
+      title: 'Pseudo elements',
+      contents: [
+        {
+          label: '::before & ::after'
+        },
+        {
+          label: '::first-line, ::first-letter, ::marker, ::selection'
+        }
+      ]
+    },
+    {
+      title: 'Pseudo Classes',
+      contents: [
+        {
+          label: ':hover, :focus'
+        },
+        {
+          label: ':active, :visited, :disabled'
+        },
+        {
+          label: ':nth-child, :nth-of-type, :target'
+        },
+        {
+          label: ':checked'
+        }
+      ]
+    },
+    {
+      title: "Typography - em, rem, ch, px",
+    },
+    {
+      title: "Box-sizing",
+    },
+    {
+      title: "Grid System",
+      contents: [
+        {
+          label: "Flex/Grid",
+        },
+        {
+          label: 'Exercise',
+          link: 'https://codepen.io/koushik27/pen/QWpwYYw?editors=1100'
+        },
+        {
+          label: "Create your own",
+          link: "https://codepen.io/koushik27/pen/JjWoMog",
+        },
+      ],
     },
   ];
   return (
