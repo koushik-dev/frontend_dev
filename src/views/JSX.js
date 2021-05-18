6be75fb0d5c5d284d10740f3a7121c3dc10b943d
Koushik <rskkoushik@gmail.com>
2021-05-18 21:40:01 +0530

    fix: jsx

--- src/views/JSX.js
+++ src/views/JSX.js
@@ -3,53 +3,77 @@ import Details from "../shared/Details";
 
 const JSX = () => {
   const contents = [
-      {
-          title: 'Mechanism',
-          contents: [
-              {
-                  label: 'Rendering'
-              },
-              {
-                  label: 'Virtual DOM, Change Detection, Diffing Algorithm'
-              }
-          ]
-      },
-      {
-          title: 'Routing',
-          contents: [
-            {
-              label: 'Basics',
-              link: 'https://reactrouter.com/web/guides/quick-start'
-            }, {
-              label: 'Simple Implementation',
-              link: 'https://ui.dev/build-your-own-react-router-v4/'
-            }
-          ]
-      },
-      {
-        title: 'Hooks',
-        contents: [
-          {
-            label: 'useState'
-          },
-          {
-            label: 'UseEffect'
-          },
-          {
-            label: 'References',
-            link: 'https://reactjs.org/docs/hooks-reference.html'
-          }
-        ]
-      },
-      {
-          title: 'Async calls'
-      },
-      {
-          title: 'JSX, Typescript'
-      },
-      {
-          title: 'Tools'
-      }
+    {
+      title: "Mechanism",
+      contents: [
+        {
+          label: "Rendering",
+        },
+        {
+          label: "Virtual DOM, Change Detection, Diffing Algorithm",
+        },
+      ],
+    },
+    {
+      title: "Routing",
+      contents: [
+        {
+          label: "Basics",
+          link: "https://reactrouter.com/web/guides/quick-start",
+        },
+        {
+          label: "Simple Implementation",
+          link: "https://ui.dev/build-your-own-react-router-v4/",
+        },
+      ],
+    },
+    {
+      title: "Hooks",
+      contents: [
+        {
+          label: "useState",
+        },
+        {
+          label: "UseEffect",
+        },
+        {
+          label: "References",
+          link: "https://reactjs.org/docs/hooks-reference.html",
+        },
+      ],
+    },
+    {
+      title: "Async calls",
+    },
+    {
+      title: "JSX, Typescript",
+    },
+    {
+      title: "Tools",
+      contents: [
+        {
+          label: "Prettier",
+        },
+        {
+          label: "ESlint",
+        },
+        {
+          label: "Babel",
+        },
+        {
+          label: "Webpack",
+        },
+      ],
+    },
+    {
+      title: "References",
+      contents: [
+        {
+          label: "Complete Intro to React App",
+          link: "https://btholt.github.io/complete-intro-to-react-v6/",
+        },
+      ],
+    },
   ];
   return (
     <main className="html">
